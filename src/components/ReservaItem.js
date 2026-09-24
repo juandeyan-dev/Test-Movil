72da8d0ff5e3afcdeadd85ad466c2c234b1eab9c
Juandeyan-dev <juandeyan.dev@gmail.com>
2026-09-24 17:26:42 -0500

    Avance con el botón para reservar

---
+++ src/components/ReservaItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Image, Pressable, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { colors, spacing, sombra, radius } from '../theme/index';
+
+export default function ReservaItem(){
+    <View>
+        <Text></Text>
+        <Pressable>
+            
+        </Pressable>
+    </View>
+};
